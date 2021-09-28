291011d8cd61bdc76b97614a1e7178d49d3fefb3
taisuke-fujimoto <taisuke.fujimoto@phoneappli.net>
2021-09-28 21:16:21 +0900

    update dependency

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,16 +1,16 @@
 // JetBrains
-const val KOTLIN_VERSION = "1.5.30"
+const val KOTLIN_VERSION = "1.5.31"
 
 // ktlint
-const val GRADLE_KTLINT_VERSION = "10.1.0"
+const val GRADLE_KTLINT_VERSION = "10.2.0"
 
 // spring
-const val SPRING_BOOT_VERSION = "2.5.4"
+const val SPRING_BOOT_VERSION = "2.5.5"
 const val SPRING_DEPENDENCY_MANAGEMENT_VERSION = "1.0.11.RELEASE"
 // 参考 https://github.com/spring-cloud/spring-cloud-release/wiki/Spring-Cloud-2020.0-Release-Notes
-const val SPRING_CLOUD_DEPENDENCIES_VERSION = "2020.0.3"
-const val SPRING_CLOUD_CONTRACT_VERSION = "3.0.3"
+const val SPRING_CLOUD_DEPENDENCIES_VERSION = "2020.0.4"
+const val SPRING_CLOUD_CONTRACT_VERSION = "3.0.4"
 
 // kotest
-const val KOTEST_VERSION = "4.6.2"
+const val KOTEST_VERSION = "4.6.3"
 const val KOTEST_SPRING_VERSION = "4.4.3"
